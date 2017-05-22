a26c6c418c8246be5bfe584d80ea67b040c11b47
zonetrooper32 <zone3692@hotmail.com>
2017-05-21 20:37:51 -0600

    upload raw implementation

---
+++ HamburgerMenuDemo/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HamburgerMenuDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SystemNavigationManager.GetForCurrentView().BackRequested += OnBackRequested;
+            Button1.IsChecked = true;
+            MainFrame.Navigate(typeof(Button1Page));
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;   // Hamburger Menu
+        }
+
+        // Back Navigation Set-up
+        private void OnBackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                e.Handled = true;
+                MainFrame.GoBack();
+            }
+            // Change selection of Hamburger Items
+            if (MainFrame.CurrentSourcePageType.Equals(typeof(Button1Page)))
+            {
+                Button1.IsChecked = true;
+            }
+            else if (MainFrame.CurrentSourcePageType.Equals(typeof(AboutPage)))
+            {
+                AboutButton.IsChecked = true;
+            }
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                    MainFrame.CanGoBack ?
+                    AppViewBackButtonVisibility.Visible :
+                    AppViewBackButtonVisibility.Collapsed;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(Button1Page));
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(typeof(AboutPage));
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# HamburgerMenuDemo
+
+Simple Windows 10 UWP Hamburger Menu Raw Implementation
+
+## Screenshot
+
+PC:
+
+
+## Original Reference
+https://maximelabelle.wordpress.com/2016/02/02/building-a-hamburger-menu-for-your-universal-app/
